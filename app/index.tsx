812b690ccdc6cf0f8e15c4900a04457d8f862747
yanqui-uxo <zdiesing@icloud.com>
2025-09-09 20:44:53 -0700

    AudioParam bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,85 @@
 import { useRef, useState } from "react";
 import { Button } from "react-native";
-import { AudioManager, AudioRecorder } from "react-native-audio-api";
+import {
+  AudioContext,
+  AudioManager,
+  AudioRecorder,
+  BaseAudioContext,
+  OfflineAudioContext,
+} from "react-native-audio-api";
 
 AudioManager.setAudioSessionOptions({
-  iosCategory: "record",
+  iosCategory: "playAndRecord",
+  iosOptions: ["defaultToSpeaker"],
 });
 AudioManager.requestRecordingPermissions();
 
-export default function Index() {
+function AudioRecorderBug() {
   const recorderRef = useRef(
     new AudioRecorder({ sampleRate: 44100, bufferLengthInSamples: 441 })
   );
-  const [isRecording, setIsRecording] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
 
   return (
     <Button
-      title={isRecording ? "Stop recording" : "Start recording"}
+      title={isStarted ? "Stop AudioRecorder" : "Start AudioRecorder"}
       onPress={() => {
-        if (isRecording) {
+        if (isStarted) {
           recorderRef.current.stop();
         } else {
           recorderRef.current.start();
         }
-        setIsRecording((b) => !b);
+        setIsStarted((b) => !b);
       }}
     />
+  );
+}
+
+function audioParamTest(ac: BaseAudioContext) {
+  const osc = ac.createOscillator();
+  osc.connect(ac.destination);
+  for (let i = 0; i < 500; i++) {
+    osc.frequency.setValueAtTime(440 + i * 2, 0.01 * i);
+  }
+  osc.start();
+  osc.stop(5);
+}
+function AudioParamBug() {
+  return (
+    <>
+      <Button
+        title="Regular audio context AudioParam test"
+        onPress={() => {
+          audioParamTest(new AudioContext());
+        }}
+      />
+      <Button
+        title="Offline audio context AudioParam test"
+        onPress={async () => {
+          const offlineAudioContext = new OfflineAudioContext({
+            sampleRate: 44100,
+            length: 44100 * 5,
+            numberOfChannels: 2,
+          });
+          audioParamTest(offlineAudioContext);
+          const buf = await offlineAudioContext.startRendering();
+
+          const audioContext = new AudioContext();
+          const node = audioContext.createBufferSource();
+          node.buffer = buf;
+          node.connect(audioContext.destination);
+          node.start();
+        }}
+      />
+    </>
+  );
+}
+
+export default function Index() {
+  return (
+    <>
+      <AudioRecorderBug />
+      <AudioParamBug />
+    </>
   );
 }
